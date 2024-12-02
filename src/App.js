36110ad48292f983db6907d5c97757ebd74c65a0
Jessica <107456694+fungjessica@users.noreply.github.com>
2024-12-02 13:25:07 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Review from './components/Review';
 import Auction from './components/Auction';
 import Shipping from './components/Shipping';
 import Notifications from './components/Notifications';
+import Login_Register from './components/Login_Register';
+import Product_Review from './components/Product_Review';
 
 function App() {
     const [activeSection, setActiveSection] = useState('home');
@@ -18,16 +20,20 @@ function App() {
     return (
         <div>
             <Navbar setActiveSection={setActiveSection} />
+            
             {activeSection === 'home' && <Home />}
+            {activeSection === 'login_register' && <Login_Register setLoggedInUserId={setLoggedInUserId} />}
             {activeSection === 'login' && <Login setLoggedInUserId={setLoggedInUserId} />}
             {activeSection === 'register' && <Register />}
             {activeSection === 'products' && <Products loggedInUserId={loggedInUserId} />}
             {activeSection === 'cart' && <Cart loggedInUserId={loggedInUserId} />}
-	    {activeSection === 'orders' && <Orders loggedInUserId={loggedInUserId} />}
+            {activeSection === 'orders' && <Orders loggedInUserId={loggedInUserId} />}
             {activeSection === 'review' && <Review />}
             {activeSection === 'auction' && <Auction />}
             {activeSection === 'shipping' && <Shipping />}
             {activeSection === 'notifications' && <Notifications />}
+            {activeSection === 'product_review' && <Product_Review loggedInUserId={loggedInUserId} />}
+
         </div>
     );
 }
