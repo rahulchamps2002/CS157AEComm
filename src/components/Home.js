5786b58dae72474dc5b92e0d28bd7d5b68129984
Jessica <107456694+fungjessica@users.noreply.github.com>
2024-12-02 17:51:49 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            <h2>Welcome to the Marketplace</h2>
-            <p>Explore our exclusive collection!</p>
+        <div className='homeContainer'>
+            <h2>eBUY, the next generation's #1 e-commerce website</h2>
+            <p>At eBUY, we believe in goodies for all. We sell products for 
+                all events and ocassions. Whatever you need, eBUY has it all!
+            </p>
         </div>
     );
 }
